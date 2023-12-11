fa3ca45248d9e082f86892190fed25401b0618ef
Antoine Aube <aaube3@gmail.com>
2023-12-11 08:01:38 -0700

    refactored put endpoint with db

--- test.py
+++ test.py
@@ -2,5 +2,20 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10})
+data = [{"likes": 10, "name": "How to make pizza", "views": 1000},
+        {"likes": 20, "name": "How to do life", "views": 300},
+        {"likes": 30, "name": "How to make pie", "views": 10000}]
+
+for i in range(len(data)):
+  response = requests.put(BASE + "video/" + str(i), data[i])
+  print(response.json())
+
+# response = requests.put(BASE + "video/3", {"likes": 10, "name": "Antoine", "views": 1000})
+# print(response.json())
+
+# response = requests.delete(BASE + "video/0")
+# print(response)
+
+input()
+response = requests.get(BASE + "video/2")
 print(response.json())
